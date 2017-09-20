bbd37eff3c68241d52d1cbb7373d7257dfac1894
tchitchikov <jinterlan5@gmail.com>
2017-09-19 20:58:30 -0400

    found the maximum subarray

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("here")
 	inputArray := []int{100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97}
-	fmt.Println(ch4.FindMaxCrossingSubarray(common.TimeSeriesMove(inputArray)))
+	fmt.Println(ch4.FindMaxCrossingSubarray(common.TimeSeriesMove(inputArray), 0, common.FindIntArrayMidpoint(inputArray), len(inputArray)))
+	fmt.Println(ch4.FindMaxSubarray(common.TimeSeriesMove(inputArray), 0, len(common.TimeSeriesMove(inputArray))-1))
 }
--- src/ch4/maximum_sub_array.go
+++ src/ch4/maximum_sub_array.go
@@ -1,29 +1,43 @@
 package ch4
 
 import (
-	"common"
-	"fmt"
+	"math"
 )
 
-func FindMaxCrossingSubarray(input []int) (maxLeft int, maxRight int, sum int) {
+func FindMaxCrossingSubarray(input []int, low int, mid int, high int) (maxLeft int, maxRight int, sum int) {
 	leftSum := 0
 	rightSum := 0
-	for iter := common.FindIntArrayMidpoint(input); iter >= 0; iter-- {
+	for iter := mid; iter >= low; iter-- {
 		sum = sum + input[iter]
-		if sum > leftSum || iter == common.FindIntArrayMidpoint(input) {
+		if sum > leftSum || iter == mid {
 			leftSum = sum
 			maxLeft = iter
 		}
 	}
 	sum = 0
-	for iter := common.FindIntArrayMidpoint(input) + 1; iter < len(input); iter++ {
+	for iter := mid + 1; iter < high-1; iter++ {
 		sum = sum + input[iter]
-		if sum > rightSum || iter == common.FindIntArrayMidpoint(input)+1 {
+		if sum > rightSum || iter == mid+1 {
 			rightSum = sum
 			maxRight = iter
-			fmt.Println(sum, rightSum, maxRight)
 		}
 	}
 	sum = leftSum + rightSum
 	return maxLeft, maxRight, sum
+}
+
+func FindMaxSubarray(input []int, low int, high int) (int, int, int) {
+	if high == low {
+		return low, high, input[low]
+	}
+	mid := int(math.Floor(float64((low + high) / 2)))
+	leftLow, leftHigh, leftSum := FindMaxSubarray(input, low, mid)
+	rightLow, rightHigh, rightSum := FindMaxSubarray(input, mid+1, high)
+	crossLow, crossHigh, crossSum := FindMaxCrossingSubarray(input, low, mid, high)
+	if leftSum >= rightSum && leftSum >= crossSum {
+		return leftLow, leftHigh, leftSum
+	} else if rightSum >= leftSum && rightSum >= crossSum {
+		return rightLow, rightHigh, rightSum
+	}
+	return crossLow, crossHigh, crossSum
 }
